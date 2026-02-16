0c1f661c6713d0b8b4a35a2ab7f407c368c055bd
Robert Fidler <robertf224@gmail.com>
2026-02-16 01:30:27 -0500

    cleanup

--- packages/ontology/src/ir/index.ts
+++ packages/ontology/src/ir/index.ts
@@ -1,6 +1,1 @@
-export * from "./types.js";
-export { o } from "./builders.js";
-export * from "./validate.js";
-export { default as ontologyIRSchema } from "./schema.js";
-export { default as ontologyIR } from "./ontology.js";
-export * from "./toSchemaIR.js";
+export * from "./ontology.js";
--- packages/schema/src/ir/index.ts
+++ packages/schema/src/ir/index.ts
@@ -1,3 +1,3 @@
 export * from "./types.js";
 export { s } from "./builders.js";
-export { default as SchemaIR } from "./schema.js";
+export { default as SchemaIRSchema } from "./schema.js";
