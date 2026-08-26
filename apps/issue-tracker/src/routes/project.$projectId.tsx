373bca81c4451ee4ffcb25eb8f84d7f354e1acaf
Robert Fidler <robertf224@gmail.com>
2026-08-26 14:08:27 -0400

    client fixes in issue-traacker

--- apps/issue-tracker/src/routes/project.$projectId.tsx
+++ apps/issue-tracker/src/routes/project.$projectId.tsx
@@ -1,11 +1,30 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { IssueTracker } from "../app/IssueTracker";
+import {
+    createFileRoute,
+    lazyRouteComponent,
+} from "@tanstack/react-router";
+
+const IssueTracker =
+    lazyRouteComponent(
+        () =>
+            import(
+                "../app/IssueTracker"
+            ),
+        "IssueTracker"
+    );
 
 export const Route = createFileRoute("/project/$projectId")({
+    ssr: false,
     component: ProjectRoute,
 });
 
 function ProjectRoute() {
-    const { projectId } = Route.useParams();
-    return <IssueTracker initialProjectId={projectId} />;
+    const { projectId } =
+        Route.useParams();
+    return (
+        <IssueTracker
+            initialProjectId={
+                projectId
+            }
+        />
+    );
 }
